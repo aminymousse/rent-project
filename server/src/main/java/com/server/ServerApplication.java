ff4d729245a2cd620ca663475d217ae04308a2c4
Amine Saltani <aminesaltani@Amines-MacBook-Pro.local>
2025-02-18 15:44:52 +0100

    Initial commit

---
+++ server/src/main/java/com/server/ServerApplication.java
@@ -0,0 +1,17 @@
+package com.server;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+
+}
